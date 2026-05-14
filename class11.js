a41a0fb1043e64ed3e1189ced72b7c1aae66e9d7
Vikas Bijanalli <vikas.bijanalli@globallogic.com>
2026-05-14 12:20:22 +0530

    Add class11.js with basic JavaScript examples

---
+++ class11.js
@@ -0,0 +1,22 @@
+console.log("hello world")
+
+let a = 10;
+console.log(typeof (a))
+console.log(!a)
+
+let b = 10.7;
+console.log(typeof (b))
+
+var c = 30;
+console.log("c value is " + typeof (c))
+
+let d = true;
+console.log(typeof (d))
+
+console.log(!d)
+
+let e
+console.log(typeof (e))
+
+var c = a + b;
+console.log("new c values is  " + c)
