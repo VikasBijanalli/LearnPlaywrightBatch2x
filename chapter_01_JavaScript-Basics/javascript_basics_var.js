c86a30c27743fd750f44f5c7bccafe8deec9b998
Vikas Bijanalli <vikas.bijanalli@globallogic.com>
2026-05-18 22:23:24 +0530

    Add chapter_01_JavaScript-Basics, remove old chapter_01_basics

---
+++ chapter_01_JavaScript-Basics/javascript_basics_var.js
@@ -0,0 +1,17 @@
+var a = 10
+var a = 20
+a = 100
+console.log(a)
+console.log(typeof (a))
+
+//re-initialization is possible with var
+//re-assigning is also possible with var
+
+
+console.log(userName)
+var userName = "vikas"
+
+//undefined error will give if we try to acess the value before assign
+
+
+
--- chapter_01_basics/01_basics.js
+++
@@ -1,9 +0,0 @@
-let username = "Vikas";
-let loginAttempts = 3;
-let isBlocked = false;
-
-if (loginAttempts < 5 && !isBlocked) {
-    console.log(username + " can login");
-} else {
-    console.log("Access denied");
-}
