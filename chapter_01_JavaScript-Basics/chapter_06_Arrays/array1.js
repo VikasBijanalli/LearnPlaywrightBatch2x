d29a049bfb68a52771122f4da06e3b30ca476131
Vikas Bijanalli <vikas.bijanalli@globallogic.com>
2026-06-02 22:31:03 +0530

    Add chapter_06_Arrays with array examples, fix practice.js trailing newline

---
+++ chapter_01_JavaScript-Basics/chapter_06_Arrays/array1.js
@@ -0,0 +1,6 @@
+let fruits = [];
+console.log(fruits);
+
+let count = ["one", "two", "three", "four", "five"];
+console.log(count);
+console.log(count[1]);
---
+++ chapter_01_JavaScript-Basics/chapter_06_Arrays/array_access.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+console.log(numbers[2]);
+
+numbers[0] = 10;
+
+console.log(numbers);
