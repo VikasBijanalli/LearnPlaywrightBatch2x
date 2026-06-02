0208f2063ec6ab4893ed365e7c1d10ff26593a14
Vikas Bijanalli <vikas.bijanalli@globallogic.com>
2026-06-02 23:07:27 +0530

    Add array manipulation examples (adding_remove2, another_array_forloop)

---
+++ chapter_01_JavaScript-Basics/chapter_06_Arrays/adding_remove2.js
@@ -0,0 +1,18 @@
+// let arr = [1, 2, 3];
+// console.log(arr);
+
+// arr.push(3, 4, 5);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+
+let new_arr = [1, 2, 3, 4, 5, 6];
+console.log(new_arr);
+
+new_arr.splice(2, 0, 100, 200, 300);
+console.log(new_arr);
+
+// new_arr.splice(3, 3);
+// console.log(new_arr)
---
+++ chapter_01_JavaScript-Basics/chapter_06_Arrays/another_array_forloop.js
@@ -0,0 +1,7 @@
+let browser = ["chrome", "firefox", "opera", "edge"];
+
+console.log(browser.length);
+
+for (i = 0; i < browser.length; i++) {
+    console.log(browser[i])
+}
